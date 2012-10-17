4e1160b11ce48ba3b023bb512d77212e5a7cfe99
zero <zeroppai@gmail.com>
2012-10-18 03:18:53 +0900

    ページャを作成した

--- inc_index.php
+++ inc_index.php
@@ -76,20 +76,29 @@
       <div id="content">
         <div class="mozaique">
 <?
-foreach ($items as $item) {
-  dispImageThumbnail($item);
-}
+  foreach ($items as $item) {
+    dispImageThumbnail($item);
+  }
 ?>
         </div> <!-- .mozaique -->
         
         <div class="pagination">
           <ul>
-            <li><a href="http://www.xvideos.com/new/1/">1</a></li>
-            <li><a href="http://www.xvideos.com/new/2/">2</a></li>
-            <li><a href="http://www.xvideos.com/new/3/">3</a></li>
-            <li><a class="nP" href="">/</a></li>
-            <li><a href="http://www.xvideos.com/new/100/">100</a></li>
-            <li><a class="nP" href="http://www.xvideos.com/new/1/">Next</a></li>
+<?
+  if(isset($_GET['page']) && $_GET['page']>=0){
+    if($_GET['page']>1) echo '<li><a class="nP" href="index.php?page='.($_GET['page']-1).'">Prev</a></li>';
+    foreach (range(max($_GET['page']-7,0),min($page_max-$_GET['page'],14)) as $i) {
+      echo '<li><a '.($_GET['page']==$i?'class="sel" ':'').'href="index.php?page='.$i.'">'.($i+1).'</a></li>';
+    }
+    echo '<li><a class="nP" href="">/</a></li>';
+    echo '<li><a href="index.php?page='.count($page_max).'">'.(count($page_max)+1).'</a></li>';
+    if($_GET['page']<count($page_max)) echo '<li><a class="nP" href="index.php?page='.($_GET['page']+1).'">Next</a></li>';
+  }else{
+    foreach (range(0,min($page_max,20)) as $i) {
+      echo '<li><a href="index.php?page='.$i.'">'.($i+1).'</a></li>';
+    }
+  }
+?>
           </ul>
         </div>
         
--- index.php
+++ index.php
@@ -6,7 +6,14 @@ include('functions.php');
 execute();
 
 function defaultAction(){
-	$items = getImageItemsFromBeluga();
+	if(isset($_GET['page']) && $_GET['page']>0){
+		$page_count = $_GET['page'] * 40; 
+	}else{
+		$page_count = 0;
+	}
+	$page_max = ceil(g('SELECT id FROM picture WHERE 1=1 ORDER BY id ASC')/40);
+	$items = getAll('SELECT * FROM picture WHERE 1=1 ORDER BY id DESC LIMIT '.$page_count.',40');
+
 	include('inc_index.php');
 }
 
