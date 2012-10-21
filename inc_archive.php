fb10561edbc2d06e28b9083b906110388283c0a9
zero <zeroppai@gmail.com>
2012-10-22 00:25:20 +0900

    アーカイブ一覧のページャーがバグっていたので修正した

--- inc_archive.php
+++ inc_archive.php
@@ -22,14 +22,14 @@
             if(isset($_GET['page']) && $_GET['page']>=0){
               if($_GET['page']>0) echo '<li><a class="nP" href="index.php?page='.($_GET['page']-1).'">Prev</a></li>';
               foreach (range(max($_GET['page']-7,0),min($page_max,14)) as $i) {
-                echo '<li><a '.($_GET['page']==$i?'class="sel" ':'').'href="index.php?page='.$i.'">'.($i+1).'</a></li>';
+                echo '<li><a '.($_GET['page']==$i?'class="sel" ':'').'href="index.php?action=archive&page='.$i.'">'.($i+1).'</a></li>';
               }
               echo '<li><a class="nP" href="">/</a></li>';
-              echo '<li><a href="index.php?page='.$page_max.'">'.($page_max+1).'</a></li>';
-              if($_GET['page']<$page_max) echo '<li><a class="nP" href="index.php?page='.($_GET['page']+1).'">Next</a></li>';
+              echo '<li><a href="index.php?action=archive&page='.$page_max.'">'.($page_max+1).'</a></li>';
+              if($_GET['page']<$page_max) echo '<li><a class="nP" href="index.php?action=archive&page='.($_GET['page']+1).'">Next</a></li>';
             }else{
               foreach (range(0,min($page_max,20)) as $i) {
-                echo '<li><a href="index.php?page='.$i.'">'.($i+1).'</a></li>';
+                echo '<li><a href="index.php?action=archive&page='.$i.'">'.($i+1).'</a></li>';
               }
             }
           ?>
