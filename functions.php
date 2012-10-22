2aa20f8fbd883339f8928dcc5a0a4e94148096f1
zero <zeroppai@gmail.com>
2012-10-23 02:58:07 +0900

    アップロード部分でBelugaの画像をアップする機能のUIを作成

--- functions.php
+++ functions.php
@@ -41,7 +41,7 @@ function dispHeader(){
 ?>
 <!DOCTYPE html>
 <!-- saved from url=(0023)http://www.xvideos.com/ -->
-<html class=" js no-flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths" lang="en">
+<html class=" js no-flexbox canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths" lang="ja">
 <!--<![endif]--><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <meta charset="utf-8"> 
   <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
@@ -132,14 +132,14 @@ function dispFooter(){
     <footer>
       <div class="terms">
         <p>
-          XPicts.net is a <strong>free hosting service for porn picts</strong>.
+          Xpicts.net は <strong>画像URLを共有し、スライドショーで閲覧出来る無料サイトです</strong>。
         </p>
         <div class="botLinks">
           <a href="http://beluga.fm">beluga.fm</a> - 
           <a href="http://beluga.fm/room/85V9p+nfRr51E">【18禁】エロ画像を貼るアレ</a> ...
         </div>
       </div>
-      <p class="slogan">XPicts.com - the best free porn videos on internet, 100% free.</p>
+      <p class="slogan">XPicts.com - 画像URL共有サイト</p>
     </footer>
     
   </div> <!-- #page  -->
--- inc_upload.php
+++ inc_upload.php
@@ -1,12 +1,40 @@
 <?
 dispHeader();
 ?>
+<script type="text/javascript">
+  $(function(){
+    var init = function(){
+      $('#upload_tab').find('li').each(function(){
+        var e = $(this);
+        if(e.attr('class') != 'present' ){
+          $(e.find('a').attr('href')).css('display','none');
+        }
+      });
+    };
+
+    $('#upload_tab').find('li').click(function(){
+      var e = $(this);
+      $('#upload_tab').find('li').removeClass('present');
+      e.addClass('present');
+      $(e.find('a').attr('href')).css('display','block');
+      init();
+    });
+  });
+</script>
+
+<style>
+#tab-wrapper div{
+  width:500px;
+}
+#upload_tab li{
+  width: 180px;
+  float: left;
+}
+</style>
     <div id="main">
 
 
     <div id="content" class="signInUp">
-      <h2 class="blackTitle">Upload your pictures</h2>
-
       <form method="POST" id="signinForm">
         <div class="rightCol">
           <div class="tabHeaderForm notLoggedIn">
@@ -24,35 +52,43 @@ dispHeader();
         </div>
       </form>
 
-      <form id="upload" method="POST" enctype="multipart/form-data">
-        <fieldset>
-          <legend>アップロード（上限は1ファイル2MB、同時に大量にアップロードすると失敗することがあります）</legend>
-          <div class="formLine" style="margin-left:20px;">
-            <input type="checkbox" id="make_archive" onchange="$('div.archive_form').toggle();"/><span class="check_label">同時にアーカイブを作成する</span>
-            <div class="archive_form" style="display:none; margin-left:10px;">
-              <label>アーカイブ名</label>
-              <input type="text" id="archive_name" style="width:300px;" value=""/>
-            </div>
-            <div class="archive_form" style="display:none; margin-left:10px;">
-              <label>タグ(半角カンマで複数可能)</label>
-              <input type="text" id="archive_tags" style="width:200px;" value=""/>
-            </div>
-          </div>
+      <div id="tab-wrapper">
+        <ul id="upload_tab" class="redStripe">
+          <li class="present" ><a href="#uplaod1">Imgurl.comでアップロード</a></li>
+          <li><a href="#uplaod2">Beluga.fmでアップロード</a></li>
+        </ul>
+        <div id="uplaod1">
+          <form id="upload" method="POST" enctype="multipart/form-data">
+            <fieldset>
+              <legend>アップロード（上限は1ファイル2MB、1時間で50ファイルアップロード可能）</legend>
+              <div class="formLine" style="margin-left:20px;">
+                <input type="checkbox" id="make_archive" onchange="$('div.archive_form').toggle();"/><span class="check_label">同時にアーカイブを作成する</span>
+                <div class="archive_form" style="display:none; margin-left:10px;">
+                  <label>アーカイブ名</label>
+                  <input type="text" id="archive_name" style="width:300px;" value=""/>
+                </div>
+                <div class="archive_form" style="display:none; margin-left:10px;">
+                  <label>タグ(半角カンマで複数可能)</label>
+                  <input type="text" id="archive_tags" style="width:200px;" value=""/>
+                </div>
+              </div>
 
-          <input type="hidden" id="MAX_FILE_SIZE" name="MAX_FILE_SIZE" value="300000" />
-          <div>
-            <label for="fileselect">Files to upload:</label>
-            <input type="file" id="fileselect" name="fileselect[]" multiple="multiple" />
-            <div id="filedrag">or drop files here</div>
-          </div>
+              <input type="hidden" id="MAX_FILE_SIZE" name="MAX_FILE_SIZE" value="300000" />
+              <div>
+                <label for="fileselect">Files to upload:</label>
+                <input type="file" id="fileselect" name="fileselect[]" multiple="multiple" />
+                <div id="filedrag">or drop files here</div>
+              </div>
 
-          <div id="submitbutton">
-            <button type="button" id="uploadButton">Upload Files</button>
-          </div>
+              <div id="submitbutton">
+                <button type="button" id="uploadButton">Upload Files</button>
+              </div>
 
-        </fieldset>
-      </form>
-
+            </fieldset>
+          </form>
+        </div>
+        <div id="uplaod2">
+        </div>
       <div id="uploadMessages"></div>
     </div>
 
